0b767ac53b3703c6630e6673971e3c749d63e645
Kevin Mugiira <kevinmugiira@gmail.com>
2022-06-20 15:38:09 +0300

    added to the token package

--- src/main/java/com/example/springauthentication1/appuser/AppUserService.java
+++ src/main/java/com/example/springauthentication1/appuser/AppUserService.java
@@ -1,11 +1,16 @@
 package com.example.springauthentication1.appuser;
 
+import com.example.springauthentication1.registration.token.ConfirmationToken;
+import com.example.springauthentication1.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 
 @Service
@@ -17,6 +22,7 @@ public class AppUserService implements UserDetailsService {
             "user with email %s not found";
     private final AppUserRepository appUserRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ConfirmationTokenService confirmationTokenService;
 
     @Override
     public UserDetails loadUserByUsername(String email)
@@ -46,7 +52,19 @@ public class AppUserService implements UserDetailsService {
         //persisting user to the database
         appUserRepository.save(appUser);
 
-        //TODO: Send confirmation token
-        return "it works";
+        String token = UUID.randomUUID().toString();
+
+        //Send confirmation token
+        ConfirmationToken confirmationToken = new ConfirmationToken(
+                token,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(15),
+                appUser
+        );
+
+        confirmationTokenService.saveConfirmationToken(confirmationToken);
+
+        //TODO: SEND EMAIL
+        return token;
     }
 }
